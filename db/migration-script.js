b46e487bb6bedb98670f022845db4a7e2c0a967a
Brea Renee <brearenee@gmail.com>
2023-09-20 17:49:49 -0600

    package installed, migration script written

--- db/migration-script.js
+++ db/migration-script.js
@@ -1,1 +1,22 @@
 //TODO: https://www.antoniovdlc.me/a-look-at-postgresql-migrations-in-node/
+import {migrate} from "postgres-migrations";
+
+async function migrateConfig() {
+  const dbConfig = {
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USR,
+    password: process.env.POSTGRES_PWD,
+    host: process.env.POSTGRES_HOST,
+    port: POSTGRES_PORT,
+
+    // Default: false for backwards-compatibility
+    // This might change!
+    ensureDatabaseExists: true,
+
+    // Default: "postgres"
+    // Used when checking/creating "database-name"
+    defaultDatabase: "postgres"
+  }
+
+  await migrate(dbConfig, ".")
+}
