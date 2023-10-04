3bcc17fd960630bf6084d86df8bb3304ce1b78e5
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-10-04 17:40:31 -0600

    Updating test files and linting .js files I changed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,13 +21,13 @@ async function fetchAndPopulateFeed() {
                     card.querySelector("p").innerText = users.created_at;
                     card.querySelector("p1").innerText = users.body;
 
-            // Create a new row div to wrap the card
-            const colDiv = document.createElement("div");
-            colDiv.classList.add("row");
-            colDiv.classList.add("g-3");
+                    // Create a new row div to wrap the card
+                    const colDiv = document.createElement("div");
+                    colDiv.classList.add("row");
+                    colDiv.classList.add("g-3");
 
-            // Append the card to the new row div
-            colDiv.appendChild(card);
+                    // Append the card to the new row div
+                    colDiv.appendChild(card);
 
                     // Append the new column div to the row
                     feedContent.appendChild(colDiv);
