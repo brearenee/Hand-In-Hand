6b630e7b6f5aad722f6fbbf2d9e5a02cf061f9f1
Brea <brearenee@gmail.com>
2023-10-06 08:18:00 -0600

    swagger documentaiton finally working

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const app = express();
+const YAML = require('yamljs');
 require('./src/routes/routes')(app);
 const postsRoutes = require('./src/routes/post-routes.js');
 const port = process.env.PORT || 3000;
-
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger/swagger.json');
-
-
-// Serve Swagger documentation
+const swaggerDoc = YAML.load('./swagger/swagger.yaml');
+ 
 app.use(express.json());
-app.use(express.static('/api-docs'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/posts', postsRoutes);
-
-
-
 app.use(express.static('public'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
+  
