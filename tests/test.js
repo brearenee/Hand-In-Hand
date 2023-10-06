ebf81b23e6cadf9089f50b2439b7b844fb598c81
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-10-05 21:50:39 -0600

    updating test.js to also use selenium

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { Builder, By } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+const chrome = require('selenium-webdriver/chrome');
 
 describe("fetchPosts using Selenium", function() {
     // Set the timeout for this hook
@@ -10,20 +10,20 @@ describe("fetchPosts using Selenium", function() {
     before(async function() {
         
 
-        // Initialize a headless chrome driver
+        // Initialize selenium chrome driver
         driver = new Builder()
             .forBrowser("chrome")
             .setChromeOptions(new chrome.Options().headless())
             .build();
 
-        // Go to your HTML page
-        await driver.get("http://localhost:3000"); 
+        // Go to localhost to test dashboard
+        await driver.get('http://localhost:3000'); 
 
-        // For the purpose of the test, we use a free online service to mock the posts API
-        await driver.executeScript("fetchAndPopulateFeed();");
+        // For this test, we use the fetchAndPopulateFeed() function
+        await driver.executeScript(`fetchAndPopulateFeed();`);
     });
 
-    it("should fetch and populate feed content using Selenium", async function() {
+    it("should fetch and populate feed content", async function() {
         await new Promise(resolve => setTimeout(resolve, 2000));
 
         const feedContent = await driver.findElement(By.id("feed-content"));
@@ -32,7 +32,7 @@ describe("fetchPosts using Selenium", function() {
         expect(children.length).to.be.at.least(1); 
 
         const firstCardTitle = await children[0].findElement(By.css("h4")).getText();
-        expect(firstCardTitle).to.be.a("string");
+        expect(firstCardTitle).to.be.a('string');
     });
 
     after(async function() {
