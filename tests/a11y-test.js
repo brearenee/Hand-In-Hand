64ea603fe6a780479c03beb941b0bab534e2696e
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-10-05 21:01:17 -0600

    Updated tests to use selenium browser automation

--- tests/a11y-test.js
+++ tests/a11y-test.js
@@ -1,45 +1,46 @@
 const { expect } = require("chai");
-const axe = require("axe-core");
 const path = require("path");
-const fs = require("fs");
-const { JSDOM } = require("jsdom");
 
-const file = path.join(__dirname, "../public/index.html");
 
-// Utility function to run specific axe rule
-function runAxeRule(element, rule, callback) {
-    axe.run(element, { runOnly: { type: "rule", values: [rule] } }, callback);
+const { AxeBuilder } = require("@axe-core/webdriverjs");
+const { Builder } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+async function checkAccessibilityRule(driver, rule) {
+    const results = await new AxeBuilder(driver).analyze();
+    return results.violations.filter(violation => violation.id === rule);
 }
+
 
 
 describe("Accessibility Tests", function() {
     this.timeout(10000);
-  
-    const html = fs.readFileSync(file, "utf-8");
-    const { window, document } = new JSDOM(html).window;
+    let driver;
 
-
-    // check color contrast 
-    describe("Color Contrast", function() {
-        it("should meet color contrast standards", function(done) {
-            runAxeRule(document.body, "color-contrast", function(err, results) {
-                expect(results.violations.length).to.equal(0, JSON.stringify(results.violations));
-                done();
-            });
-        });
+    before(async () => {
+        driver = new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(new chrome.Options().headless())
+            .build();
+        await driver.get("http://localhost:3000/");
     });
 
-
-    // check for alt text 
-    describe("Image Alt Text", function() {
-        it("should have alt text for all images", function(done) {
-            runAxeRule(document.body, "image-alt", function(err, results) {
-                expect(results.violations.length).to.equal(0, JSON.stringify(results.violations));
-                done();
-            });
-        });
+    it("should meet color contrast standards", async () => {
+        const violations = await checkAccessibilityRule(driver, "color-contrast");
+        expect(violations.length).to.equal(0, JSON.stringify(violations, null, 2));
     });
-    
-    // add more tests for WCAG guidelines 
-        
+
+    it("should have alt text for all images", async () => {
+        const violations = await checkAccessibilityRule(driver, "image-alt");
+        expect(violations.length).to.equal(0, JSON.stringify(violations, null, 2));
+    });
+
+    // Add more tests for other rules as needed...
+
+    after(async () => {
+        if (driver) await driver.quit();
+    });
 });
+
+
+
