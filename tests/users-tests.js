c0d9c4a525a34ae8e22e8c0939c71717c86a97a8
Jessica <jgardn50@msudenver.edu>
2023-10-09 21:58:01 -0600

    Added code to remove created user after test.

--- tests/users-tests.js
+++ tests/users-tests.js
@@ -3,19 +3,30 @@ const axios = require('axios');
 const { error } = require("console");
 const { response } = require("express");
 const apiUrl = 'http://localhost:3000/users'; // Replace with your actual API endpoint URL
+require("dotenv").config();
+
+const pgp = require('pg-promise')();
+const {dbConfig}= require('../src/utils/db')
+const db = pgp(dbConfig);
+
+
+const userData = {
+    id:"00000000-0000-0000-0000-000000000000",
+    username:"testuser0",
+    last_location:"586d8255-e629-4eda-a78b-af2ac0c6a4d9",
+    created_at:"2023-10-10T02:20:06.021Z",
+    updated_at:"2023-10-10T02:20:06.021Z"
+};
 
 describe("Tests user routes", function() {
-    //before({});
-    //after({});
 
+    after(async function(){
+        //remove created user from db
+        await db.none('DELETE FROM users WHERE id = $1', [userData.id]);
+    });
+
+    // Test to create a new user
     it("Post a new user", async function(){
-        const userData = {
-            id:"00000000-0000-0000-0000-000000000000",
-            username:"testuser0",
-            last_location:"586d8255-e629-4eda-a78b-af2ac0c6a4d9",
-            created_at:"2023-10-10T02:20:06.021Z",
-            updated_at:"2023-10-10T02:20:06.021Z"
-        };
 
         axios.post(apiUrl, userData).then(response => {
             console.log('Response status:', response.status);
@@ -23,8 +34,6 @@ describe("Tests user routes", function() {
             assert.equal(response.data.id, userData.id, `does not equal testUser created.`);
         });
 
-
 });
-
 
 });
