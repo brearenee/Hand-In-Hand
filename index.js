57052b383d63be784a32061c15564a99e6a03df9
Brea Renee <brearenee@gmail.com>
2023-10-09 19:19:57 -0600

    updates to merge our branches into one

--- index.js
+++ index.js
@@ -3,30 +3,14 @@ const app = express();
 const YAML = require('yamljs');
 require('./src/routes/routes')(app);
 const postsRoutes = require('./src/routes/post-routes.js');
-const usersRoutes = require('./src/routes/user-routes.js');
 const port = process.env.PORT || 3000;
 const swaggerUi = require('swagger-ui-express');
-<<<<<<< HEAD
 const swaggerDoc = YAML.load('./swagger/swagger.yaml');
- 
+
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/posts', postsRoutes);
-app.use('/users', usersRoutes);
-=======
-const swaggerDocument = require('./src/routes/swagger.json');
-
-
-// Serve Swagger documentation
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/posts', postsRoutes);
-
-
-
->>>>>>> adb2179 (can now create posts woo)
 app.use(express.static('public'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
-  
--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -87,24 +87,12 @@ async function deletePostById(req, res) {
     }
 }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> bc3e049 (swagger documentation in full force)
 async function createPost(req, res){
     request = await req
     const { title, body, user_id, location_id, type } = req.body;
-<<<<<<< HEAD
     //default user/location Ids for beginning implementation
     const defaultUserId = await getDefaultUserId('testUser1');
     const defaultLocationId = await getDefaultLocation(39.798770010686965, -105.07207748323874)
-=======
-    //default userIds for beginning implementation
-    //TODO: query method to get the userid since theyre unique across machines
-    const defaultUserId = '5bc4f097-8924-4873-9e85-a0f1de817e18'; 
-    const defaultLocationId = '08f0cdeb-adcd-4382-98ab-920b4926fd67'; 
->>>>>>> bc3e049 (swagger documentation in full force)
 
     const userId = user_id || defaultUserId;
     const locationId = location_id || defaultLocationId;
@@ -129,7 +117,7 @@ async function getDefaultUserId(username) {
     try {
         const query = 'SELECT id FROM users WHERE username = $1';
         const result = await pool.query(query, [username]);
-    
+
         if (result.rows.length > 0) {
           return result.rows[0].id;
         } else {
@@ -146,7 +134,7 @@ async function getDefaultLocation(latitude, longitude) {
     try {
         const query = 'SELECT * FROM locations WHERE lat = $1 AND long = $2';
         const result = await pool.query(query, [latitude,longitude]);
-    
+
         if (result.rows.length > 0) {
           return result.rows[0].id;
         } else {
