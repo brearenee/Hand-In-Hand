74c8d2e601049db01f179aeb629ba6bfaa3b5526
Jessica <jgardn50@msudenver.edu>
2023-10-15 21:58:15 -0600

    Added  in timestamp logic for updating and creating timestamps

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+//Get current time in correct format
+const currentDate = new Date();
+const timestamp = currentDate.toISOString();
 
 
 // Get request for all users
@@ -48,8 +51,9 @@ const getUserByID = async (request, response) => {
 // Post request for new user
 const createUser = async (request, response) => {
     try {
-        const {id, username, last_location, created_at, updated_at} = request.body;
-        const result = await pool.query("INSERT INTO users (id, username, last_location, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *", [id, username, last_location, created_at, updated_at]);
+        //created_at and updated_at the same at user creaation. 
+        const {id, username, last_location} = request.body;
+        const result = await pool.query("INSERT INTO users (id, username, last_location, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *", [id, username, last_location, timestamp, timestamp]);
         response.status(200).json(result.rows);
 
     } catch (error) {
@@ -74,10 +78,12 @@ const deleteUserByID = async (request, response) => {
 
 const updateUser = async (request, response) =>{
     try{
-        const {id, username, last_location, created_at, updated_at} = request.body;
+        const {id, username, last_location, created_at} = request.body;
         const userId = request.params.userId;
+        
+        //Update updated_at timestamp
         const result = await pool.query("UPDATE users SET username = $2, last_location = $3, created_at = $4, updated_at = $5 WHERE id = $1 RETURNING *",
-        [id, username, last_location, created_at, updated_at]);
+        [id, username, last_location, created_at, timestamp]);
 
         response.status(200).json(result.rows);
     } catch {
