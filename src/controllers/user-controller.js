d63d813c2751177001ec5b49f8d93b8101461f03
Jessica <jgardn50@msudenver.edu>
2023-10-18 15:00:59 -0600

    Update comments for update request

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -80,36 +80,46 @@ const updateUser = async (request, response) =>{
     try{
         const userId = request.params.userId;
 
+        // get user information from database to be able o update
         const userResult = await pool.query("SELECT * FROM users WHERE id = $1", [userId]);
 
+        // Check if user is in database
         if (userResult.rows.length === 0) {
             response.status(404).send("User not found");
             return;
         }
 
+        // Use reponse from user and update username and/or last_location and updated_at
         const userBody = userResult.rows[0];
-
         const newUserName = request.query.username;
         const newLastLocation = request.query.last_location;
+
+        //Update updated_at timestamp
         userBody.updated_at = timestamp;
         
+        // Update both username and last_location
         if (newUserName && newLastLocation){
             userBody.username = newUserName;
             userBody.last_location = newLastLocation;
         }
+
+        // Update just username
         else if (newUserName){
             userBody.username = newUserName;
         }
+
+        //Update just last_location
         else if (newLastLocation){
             userBody.last_location = newLastLocation;
         }
         
-        //Update updated_at timestamp
+        // Http Put request to update user with new user information
         const result = await pool.query("UPDATE users SET username = $2, last_location = $3, created_at = $4, updated_at = $5 WHERE id = $1 RETURNING *",
         [userBody.id, userBody.username, userBody.last_location, userBody.created_at, userBody.updated_at]);
-
         response.status(200).json(result.rows);
+
     } catch (error) {
+        // Log errors
         console.log("Error creating user:", error);
         console.log(request.body);
         response.status(500).send("Internal Server Error");
